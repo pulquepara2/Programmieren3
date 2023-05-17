2d775a0efb993940768ff89b4f2846b1d2367d3a
pulquepara2 <veronika.morin@hotmail.com>
2023-05-17 11:37:56 +0200

    neu

---
+++ src/Text_Output/Ue10.java
@@ -0,0 +1,31 @@
+package Text_Output;
+
+import java.io.*;
+
+public class Ue10 {
+    public static void main(String[] args) throws IOException {
+
+        //einlesen
+        InputStreamReader fr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(fr);
+        //Schreiben auf file
+        File f = new File("C:\\Users\\s53033\\meineNoten.txt");
+        FileWriter fw = new FileWriter(f);
+        PrintWriter pw = new PrintWriter(fw);
+        String zeile = br.readLine();
+        while (zeile != null) {
+            if (zeile.equals("stop")) {
+                break;
+            }
+            System.out.println(zeile);
+            pw.println(zeile);
+            pw.flush();
+            zeile = br.readLine();
+
+
+        }
+        pw.flush();
+        br.close();
+
+    }
+}
--- src/Text_Output/Ue9.java
+++ src/Text_Output/Ue9.java
@@ -5,12 +5,14 @@ import java.io.*;
 public class Ue9 {
     public static void main(String[] args) throws IOException {
         File f = new File("C:\\Users\\s53033\\eingabetext.txt");
-        FileWriter fw = new FileWriter(f);
+        FileWriter fw = new FileWriter(f, true);
+        //erst jetzt exisitert das file, aber es ist noch leer
         PrintWriter pw = new PrintWriter(fw);
-        pw.println("Das ist die erste Zeile");
-        pw.println("Das ist die zweite Zeile!!");
-
+        pw.println("Das ist die dritte Zeile");
+        pw.println("Das ist die vierte Zeile mit append:true angefügt!!");
+        pw.flush();
         pw.close();
+        //wenn ich close, wird automatisch geflusht, aber wir schreiben schön und deswegen auch flush
     }
 }
 
