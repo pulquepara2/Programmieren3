6175750e236a4d63f41ec7ae1c70b4d4dbfb94f8
pulquepara2 <veronika.morin@hotmail.com>
2023-06-26 10:08:09 +0200

    neu

---
+++ src/Network/Beispiel2/SetClientFields.java
@@ -0,0 +1,23 @@
+package Network.Beispiel2;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class SetClientFields {
+
+    public static void main(String[] args) throws IOException {
+
+        URL urlObj = new URL("https://www.google.com");
+        URLConnection urlCon = urlObj.openConnection();
+
+        urlCon.setRequestProperty("User-Agent", "Java Client; Mac OS ich sascha");
+        urlCon.setRequestProperty("Accept", "text/html");
+        urlCon.setRequestProperty("Accept-Language", "en-US");
+        urlCon.setRequestProperty("Connection", "close");
+
+        System.out.println(urlCon.getRequestProperties());
+
+
+    }
+}
